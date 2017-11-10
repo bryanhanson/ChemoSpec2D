29316a8f550f2b26cf8b8f2c635d5f74d95ce589
Bryan Hanson <hanson@depauw.edu>
2017-11-09 20:59:31 -0500

    greatly improved plotSpectra2D + MUD1

--- R/ChemoSpec2D-package.R
+++ R/ChemoSpec2D-package.R
@@ -2,48 +2,63 @@
 #'
 #'
 #' Exploratory Chemometrics for 2D Spectroscopy
-#' 
+#'
 #' A collection of functions for working with 2D spectra,
 #' developed with NMR in mind, but other types of 2D spectroscopy may also
 #' be analyzed. ChemoSpec2D takes many of its cues from ChemoSpec
 #' and tries to create consistent graphical output and be very user friendly.
-#' Tools to plot 2D spectra and carry out PARAFAC are the main features. 
-#' 
+#' Tools to plot 2D spectra and carry out PARAFAC are the main features.
+#'
 #' @name ChemoSpec2D-package
-#' 
+#'
 #' @aliases ChemoSpec2D-package ChemoSpec2D
-#' 
+#'
 #' @docType package
-#' 
+#'
 #' @author Bryan A. Hanson.
-#' 
+#'
 #' Maintainer: Bryan A. Hanson \email{hanson@@depauw.edu}
-#' 
+#'
 #' @keywords package
 #'
 #' @examples
-#' if (!requireNamespace("ChemoSpec2Ddata", quietly = TRUE)) {
-#'   stop("You must install ChemoSpec2Ddata to run this example")
+# \dontrun{
+# if (!requireNamespace("ChemoSpec2Ddata", quietly = TRUE)) {
+#   stop("You must install ChemoSpec2Ddata to run this example")
+# }
+#
+# # ChemoSpec2Ddata can be installed following the directions here:
+# # https://github.com/bryanhanson/ChemoSpec2Ddata
+# data("Esters", package = "ChemoSpec2Ddata")
+# sumSpectra2D(Esters)
+# sumGroups2D(Esters)
+# chkSpectra2D(Esters, confirm = TRUE)
+#
+# Esters2 <- removeGroup2D(Esters, "butyl") # remove the butyl esters
+# sumGroups2D(Esters2)
+#
+# Esters3 <- removeSample2D(Esters, "Pro") # remove the propanoates
+# sumSpectra2D(Esters3)
+#
+# Esters4 <- normSpectra2D(Esters)
+#
+# plotSpectra2D(Esters)
+# }
+#
+#' data(MUD1)
+#' sumSpectra2D(MUD1)
+#' sumGroups2D(MUD1)
+#' chkSpectra2D(MUD1, confirm = TRUE)
+#'
+#' mud2 <- removeGroup2D(MUD1, "GroupA")
+#' sumGroups2D(mud2)
+#'
+#' mud3 <- removeSample2D(MUD1, "Sample1")
+#' sumSpectra2D(mud3)
+#'
+#' mud4 <- normSpectra2D(MUD1)
+#'
+#' plotSpectra2D(MUD1)
 #' }
-#'
-#' # ChemoSpec2Ddata can be installed following the directions here:
-#' # https://github.com/bryanhanson/ChemoSpec2Ddata
-#'
-#' data("Esters", package = "ChemoSpec2Ddata")
-#' sumSpectra2D(Esters)
-#' sumGroups2D(Esters)
-#' chkSpectra2D(Esters, confirm = TRUE)
-#'
-#' Esters2 <- removeGroup2D(Esters, "butyl") # remove the butyl esters
-#' sumGroups2D(Esters2)
-#'
-#' Esters3 <- removeSample2D(Esters, "Pro") # remove the propanoates
-#' sumSpectra2D(Esters3)
-#'
-#' Esters4 <- normSpectra2D(Esters)
-#'
-#' plotSpectra2D(Esters)
-#'
+
 NULL
-
-
--- R/plotSpectra2D.R
+++ R/plotSpectra2D.R
@@ -24,9 +24,9 @@
 #'
 #' @seealso Please see \code{\link{ChemoSpec2D-package}} for examples.
 #' 
-#' @export plotSpectra2D
+#' @export
 #'
-#' @importFrom graphics abline contour rect
+#' @importFrom lattice contourplot lattice.getOption
 #'
 plotSpectra2D <- function(spectra, which = 1, lvls = NULL, ...) {
 	
@@ -35,11 +35,41 @@ plotSpectra2D <- function(spectra, which = 1, lvls = NULL, ...) {
   chkSpectra2D(spectra)
 
   M <- spectra$data[[which]]
+  M <- t(M[nrow(M):1,]) # 90 cw prior to rotation in contourplot
   
   if (is.null(lvls)) lvls <- guessLvls(M)
-    
-  contour(x = spectra$F2, y = spectra$F1, z = M,
-    levels = lvls, drawlabels = FALSE,...)
-    
+
+# Helper function from https://stackoverflow.com/a/47210555/633251
+# Puts F1 axis on right side by changing defaults
+  myaxis <- function(side, labels = "default", ticks = "default", ...) {
+    default <- lattice.getOption("default.args")$axis
+    if (side %in% c("right","bottom")) {
+      default(side = side, labels = "yes", ticks = "yes", ...)
+    } else {
+      default(side = side, labels = "no", ticks = "yes", ...)
+    }
+  }
+
+  # Compute tick positions and limits
+  # Little tweaks from concept testing
+  F2st <- spectra$F2[1]
+  F2end <- spectra$F2[length(spectra$F2)] + 1
+  F2at <- seq(F2st, F2end, 1)
+  F2lab <- as.character(seq(F2end, F2st, -1))
+  F1st <- spectra$F1[1]
+  F1end <- spectra$F1[length(spectra$F1)]
+  F1at <- seq(F1st, F1end, 1)
+  F1lab <- as.character(seq(F1end, F1st, -1))
+  
+  p <- contourplot(M, asp = 1,
+    xlab = spectra$unit[1], ylab = "", ylab.right = spectra$unit[2],
+    axis = myaxis,
+    scales = list(
+      x = list(at = F2at, labels = F2lab),
+      y = list(at = F1at + 1, labels = F1lab),
+      ...)
+    )
+  print(p)
+  
   invisible(lvls)
 }
