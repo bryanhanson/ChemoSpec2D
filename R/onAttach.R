9eec67b77c5934e4515be4a7bf8c0498871081a2
Bryan <hanson@depauw.edu>
2021-09-25 08:35:00 -0700

    improve onAttach message

--- R/onAttach.R
+++ R/onAttach.R
@@ -6,6 +6,7 @@
   chkReqGraphicsPkgs("ggplot2")
 
   packageStartupMessage("\nAs of version 6, ChemoSpec2D offers new graphics output options")
+  packageStartupMessage("\nFunctions plotScores and plotScree will work with the new options")
   packageStartupMessage("For details, please see ?GraphicsOptions")
   packageStartupMessage("\nThe ChemoSpec graphics option is set to 'ggplot2'")
   packageStartupMessage("To change it, do\n\toptions(ChemoSpecGraphics = 'option'),\n\twhere 'option' is one of 'base' or 'ggplot2' or'plotly'.")
